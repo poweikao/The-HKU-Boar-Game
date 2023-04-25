f0061ef1bd39090c4adbc4d88b0a5b76f84da74f
PaulK <powei@hku.hk>
2023-04-26 02:04:54 +0800

    Playable minesweeper game draft

--- minesweeper.h
+++ minesweeper.h
@@ -7,5 +7,12 @@
 #include <cstdlib>
 
 int minesweeper(); // Do not change name. This function is used in main.cpp
+bool is_mine(int row, int col, const char board[][5]);
+void make_move(int *x, int *y);
+void implement_mines(char answer_board[][5]);
+void implement_numbers(char answer_board[][5]);
+bool reveal_board(const int &r, const int &c, const char answer_board[][5], char game_board[][5]);
+bool double_check_gameend(const char answer_board[][5], const char game_board[][5]);
+void print_board(char game_board[][5]);
 
 #endif
