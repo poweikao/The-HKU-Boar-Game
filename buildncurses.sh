83aa1919b8143921920de2fb87b97211dca00765
PaulK <powei@hku.hk>
2023-05-05 20:46:29 +0800

    It turns out I didn't include buildncurses.sh, lol

---
+++ buildncurses.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# If it is not downloaded
+if ! test -d ncurses; then
+        git clone https://github.com/mirror/ncurses.git
+fi
+
+if ! test -d obj; then
+        mkdir obj
+fi
+
+if ! test -d bin; then
+        mkdir bin
+fi
+
+
+# Check for compiled ncurses
+if test -f "ncurses/.ncurses_ready"; then
+
+        # Validate whether the compiled binary is appliable
+        if [[ $(cat ncurses/.ncurses_ready) != $(uname -a)  ]]; then
+                clear
+                echo "Recompiling ncurses required."
+                echo "Please run make again by typing making again."
+                echo "Press any key to continue"
+                read X
+                make clean_all
+        fi
+
+        echo "ncurses is ready"
+        exit 0;
+fi
+
+cd ncurses
+# Referred from https://stackoverflow.com/questions/60325432/using-ncurses-without-installing-it
+
+if ! test -f .config; then
+        ./configure --prefix $(pwd)/../local --enable-widec --with-pthread --cache-file=.config
+fi 
+
+make
+make install
+
+echo $(uname -a) > ".ncurses_ready"
